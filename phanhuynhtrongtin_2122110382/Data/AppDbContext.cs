e71414d8735d400a384290470eb13d17765b4438
phanhuynhtrongtin <ptin07708@gmail.com>
2025-03-28 16:21:49 +0700

    Initial commit

---
+++ phanhuynhtrongtin_2122110382/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using phanhuynhtrongtin_2122110382.Model;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace phanhuynhtrongtin_2122110382.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+
+}
---
+++ phanhuynhtrongtin_2122110382/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace phanhuynhtrongtin_2122110382.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image{ get; set; }
+        public string Price { get; set; }
+       
+    }
+}
